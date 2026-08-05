cc96135a53284047e30ae7f058ba2f7981538554
havinaccount <203082879+havinaccount@users.noreply.github.com>
2026-08-05 17:02:46 +0330

    Some changes (Go blame it)

--- src/hidden/main.py
+++ src/hidden/main.py
@@ -3,10 +3,7 @@ from tkinter import ttk
 
 root: tk.Tk = tk.Tk()
 
-btn1: ttk.Button = ttk.Button(text = "Nigger")
+btn1: ttk.Button = ttk.Button(text="Nigger")
 btn1.pack()
 
 root.mainloop()
-
-if __name__ == "__main__":
-    main()
