93de62a8a0d0be20fe960b9275a663cb411e38a1
MMDAM2 <amirmohammadghasemi89@gmail.com>
2026-08-05 14:35:47 +0330

    New Example

--- src/hidden/main.py
+++ src/hidden/main.py
@@ -1,9 +1,11 @@
+from ast import main
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 root: tk.Tk = tk.Tk()
 
-btn1: ttk.Button = ttk.Button(text = "Nigger")
+btn1: ttk.Button(root, text = "Nigga run!", command = lambda: messagebox.showinfo("Hey nigga", "Run becuase white are cooming!")) # type: ignore
 btn1.pack()
 
 root.mainloop()
