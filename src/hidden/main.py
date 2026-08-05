51a72c97c791745d396390a22e414d98d509d71c
havinaccount <203082879+havinaccount@users.noreply.github.com>
2026-08-05 12:44:31 +0330

    Small Example

--- src/hidden/main.py
+++ src/hidden/main.py
@@ -1,6 +1,12 @@
-def main() -> None:
-    print("Hello from hidden!")
+import tkinter as tk
+from tkinter import ttk
 
+root: tk.Tk = tk.Tk()
+
+btn1: ttk.Button = ttk.Button(text = "Nigger")
+btn1.pack()
+
+root.mainloop()
 
 if __name__ == "__main__":
     main()
