4c08484519744294014d72fea863ec2a5dfeacfb
MMDAM2 <amirmohammadghasemi89@gmail.com>
2026-08-06 17:25:42 +0330

    added some tkinter

--- src/hidden/main.py
+++ src/hidden/main.py
@@ -5,16 +5,104 @@ from tkinter import (  # DO NOT REMOVE 'filedialog', it's needed for future file
     ttk,
 )
 
-root: tk.Tk = tk.Tk()
-root.withdraw()
+
+def organize_file():
+    root.withdraw()
+
+    def back():
+        window.destroy()
+        root.deiconify()
+
+    window = tk.Tk()
+    window.title()
+    window.geometry("400x150")
+    window.resizable(width=False,height=False)
+    window.protocol("WM_DELETE_WINDOW", window.quit)
+
+    helper = tk.Label(
+        window,
+        text= "Enter your file (path) also you can with ',' enter multiple files",
+        fg= "gray"
+    )
+    helper.pack_configure(pady=1)
+
+    get = ttk.Entry(
+            window,
+            width=40
+        )
+    get.pack_configure(pady = 5)
+
+    org_button = tk.Button(
+        window,
+        text= "organize file / files",
+        bg= "green"
+        #, command= 
+    )
+    org_button.pack_configure(pady= 5)
+
+    back_button = tk.Button(
+        window,
+        text= "Return to main menu",
+        bg= "red",
+        command= back
+    )
+    back_button.pack_configure(pady= 5)
+
+    window.mainloop()
+
+def organize_folder():
+    root.withdraw()
+
+    def back():
+        window.destroy()
+        root.deiconify()
+
+    window = tk.Tk()
+    window.title()
+    window.geometry("400x150")
+    window.resizable(width=False, height=False)
+    window.protocol("WM_DELETE_WINDOW", window.quit)
+
+    helper = tk.Label(
+        window,
+        text= "Enter your folder (path) also you can with ',' enter multiple folder",
+        fg = "gray"
+    )
+    helper.pack_configure(pady= 1)
+
+    get = ttk.Entry(
+        window,
+        width=40
+    )
+    get.pack_configure(pady= 5)
+
+    org_button = tk.Button(
+        window,
+        text= "Oganize folder / folders",
+        bg= "green"
+        #, command=
+    )
+    org_button.pack_configure(pady= 5)
+
+    back_button = tk.Button(
+        window,
+        text= "Return to menu",
+        bg= "red",
+        command= back
+    )
+    back_button.pack_configure(pady= 5)
+
+    window.mainloop()
+
+root: ttk.Tk = tk.Tk()
 root.title("File Organizer")
-root.geometry("300x150")
+root.geometry("400x150")
 root.resizable(width=False, height=False)
 
 btn1: ttk.Button = ttk.Button(
     root,
     text="Select Folder",
-    command=lambda: messagebox.showinfo("Not Available", "Not available for use rn"),
+    command=organize_file,
 )
 # cSpell: words padx pady
 btn1.pack_configure(padx=5, pady=5)
@@ -22,8 +110,15 @@ btn1.pack_configure(padx=5, pady=5)
 btn2: ttk.Button = ttk.Button(
     root,
     text="Organize the folder",
-    command=lambda: messagebox.showinfo("Not available", "Not available for use rn"),
+    command=organize_folder
 )
 btn2.pack_configure(padx=5, pady=5)
+
+credit = tk.Label(
+    root,
+    text= "Made By :\nMobin Saghebi\nMMDAM2",
+    fg= "gray"
+)
+credit.pack_configure(pady= 10)
 
 root.mainloop()
---
+++ temp.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def mmd():
+    root.withdraw()
+    windowmd.deiconify()
+
+    def back():
+        windowmd.withdraw()
+        root.deiconify()
+
+    bb = tk.Button(
+        windowmd,
+        text= "Return to menu",
+        bg= "red",
+        command= back
+    )
+    bb.pack_configure()
+
+    kar_ha = tk.Label(
+        windowmd,
+        text= "Nothing..."
+    )
+    kar_ha.pack_configure(pady= 5)
+
+def mobin():
+    root.withdraw()
+    windowm.deiconify()
+
+    def back():
+        windowm.withdraw()
+        root.deiconify()
+
+    bb = tk.Button(
+        windowm,
+        text= "Return to menu",
+        bg= "red",
+        command= back
+    )
+    bb.pack_configure()
+
+    kar_ha = tk.Label(
+        windowm,
+        text= " برو و اون کامند های دف فایل و فولدر رو انجام بده\nچون من نمیدونم چه دفی میخوای بزاری و همینطور \nتوهم برو برام بنویس چیکار کنم"
+    )
+    kar_ha.pack_configure(pady= 5)
+
+root = tk.Tk()
+root.title("کار هایی که باید انجام بدی")
+root.geometry("500x150")
+root.protocol("WM_DELETE_WINDOW", root.quit)
+
+windowmd = tk.Tk()
+windowmd.title("MMD")
+windowmd.geometry("500x500")
+windowmd.withdraw()
+windowmd.protocol("WM_DELETE_WINDOW", windowmd.quit)
+
+windowm = tk.Tk()
+windowm.title("Mobin")
+windowm.geometry("500x500")
+windowm.withdraw()
+windowm.protocol("WM_DELETE_WINDOW", windowm.quit)
+
+b1 = ttk.Button(
+    root,
+    text= "کار های MMD",
+    command= mmd
+)
+b1.pack_configure(pady= 10)
+
+b2 = ttk.Button(
+    root,
+    text= "کار های mobin",
+    command= mobin
+)
+b2.pack_configure(pady= 10)
+
+root.mainloop()
+windowmd.mainloop()
+windowm.mainloop()
